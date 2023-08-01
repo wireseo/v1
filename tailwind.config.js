410e599d894d8bbab9003bee346ccd45122d80f9
erynseo <yeryeon@gmail.com>
2023-08-01 10:01:29 +0800

    Base color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      "blue-black": "#0f182a",
+      "bright-blue": "#38bdf8",
+      "slate-gray": "#d7dde6",
+      "bright-gray": "#e2e8f0",
+      "dark-gray": "#64748b",
+    },
     extend: {},
   },
   plugins: [],
