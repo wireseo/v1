7c27e2cc5b9fc6dbd257049836eec7f9fb673559
erynseo <yeryeon@gmail.com>
2023-08-03 18:11:19 +0800

    Fix animation on layout sidenav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,37 @@
 import * as React from "react";
+import { useState } from "react";
+
 import { Link } from "gatsby";
+
+const Tab = ({ to, text }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <li
+      className="flex items-center w-1/6"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div
+        className={`mr-4 h-px ${
+          isHovered ? "w-12" : "w-4"
+        } bg-bright-gray transition-width duration-300 ease-in-out`}
+      ></div>
+      <Link to={to} className="text-bright-gray">
+        {text}
+      </Link>
+    </li>
+  );
+};
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className="bg-blue-black h-screen w-screen">
       <nav className="pl-16 pt-8">
         <ul className="flex flex-col space-y-4">
-          <li className="flex items-center">
-            <div className="mr-4 h-px w-12 bg-bright-gray"></div>
-            <Link to="/about" className="text-bright-gray">
-              about
-            </Link>
-          </li>
-          <li>
-            <Link to="/experience" className="text-bright-gray">
-              experience
-            </Link>
-          </li>
-          <li>
-            <Link to="/projects" className="text-bright-gray">
-              projects
-            </Link>
-          </li>
+          <Tab to="/about" text="about" />
+          <Tab to="/experience" text="experience" />
+          <Tab to="/projects" text="projects" />
         </ul>
       </nav>
 
