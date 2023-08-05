a5304e53a898e02bf6ef6acca5ea9f03bbe2d779
erynseo <yeryeon@gmail.com>
2023-08-05 11:06:09 +0800

    Fix unrecognized roboto slab

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,2 @@
 import "./src/styles/global.css";
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      key="google-fonts"
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
-    />,
-  ]);
-};
+import "typeface-roboto-slab";
