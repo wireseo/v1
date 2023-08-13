21201e2ca020f0b1d094022449613b18830d4518
erynseo <yeryeon@gmail.com>
2023-08-13 11:48:45 +0800

    Add padding to see scrolling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,15 @@ import Seo from "../components/seo";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <section id="about" className="text-[#FFFFFF]">
+      <section id="about" className="text-[#FFFFFF] p-48">
         Here is the about{/* your about section content */}
       </section>
 
-      <section id="experience" className="text-[#FFFFFF]">
+      <section id="experience" className="text-[#FFFFFF] p-48">
         Here is the experience{/* your experience section content */}
       </section>
 
-      <section id="projects" className="text-[#FFFFFF]">
+      <section id="projects" className="text-[#FFFFFF] p-48">
         Here is the projects{/* your projects section content */}
       </section>
     </Layout>
