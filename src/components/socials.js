9f4a09bcecac43eea9a66d5136b9f275ab101e12
erynseo <yeryeon@gmail.com>
2023-08-17 10:30:54 +0800

    Add hover effect for social icons

--- src/components/socials.js
+++ src/components/socials.js
@@ -13,7 +13,7 @@ const SocialIcon = ({ iconSrc, altText, profileLink }) => {
     <img
       src={iconSrc}
       alt={altText}
-      className="w-5 h-5 icon cursor-pointer fill-current text-white hover:text-bright-blue"
+      className="w-5 h-5 icon cursor-pointer fill-current text-white hover:text-bright-blue transition-transform transform-gpu hover:scale-125"
       onClick={openProfile}
     />
   );
