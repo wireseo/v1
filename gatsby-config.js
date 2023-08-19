ee9690aa840694dd6853b33cd3c0134304811226
erynseo <yeryeon@gmail.com>
2023-08-19 08:52:17 +0800

    Add smooth scroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,6 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
     `gatsby-plugin-anchor-links`,
+    `gatsby-plugin-smoothscroll`,
   ],
 };
--- src/components/tab.js
+++ src/components/tab.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useState } from "react";
-import { Link } from "gatsby";
+import scrollTo from "gatsby-plugin-smoothscroll";
 
 const Tab = ({ to, text }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -16,12 +16,14 @@ const Tab = ({ to, text }) => {
           isHovered ? "w-16 bg-bright-gray" : "w-6 bg-dark-gray"
         } transition-width duration-300 ease-in-out`}
       ></div>
-      <a
-        href={to}
-        className={`${isHovered ? ` text-bright-gray` : `text-dark-gray`}`}
+      <button
+        className={`btn-link ${
+          isHovered ? "text-bright-gray" : "text-dark-gray"
+        }`}
+        onClick={() => scrollTo(to)}
       >
         {text}
-      </a>
+      </button>
     </li>
   );
 };
