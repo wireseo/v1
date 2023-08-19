3423e1443807babf3449d2e143a13d7342f62e9f
erynseo <yeryeon@gmail.com>
2023-08-19 08:58:31 +0800

    Add min width and proper padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,43 @@ import Seo from "../components/seo";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <section id="about" className="text-[#FFFFFF] p-48 font-light">
-        ABOUT <br />
-        My problem-solving journey began unexpectedly when I picked up Java as
-        an attempt to assist my high school principal with class scheduling. I
-        bore witness to the magic of what used to be months of intense manual
-        labor being reduced to a simple—and oh so satisfying—click of a button;
-        seeing how elegant code could efficiently streamline processes, I was
-        instantly hooked to the world of code. Since then, I've ventured into
-        the dynamic realm of startups, from the buzzing landscape of{" "}
-        <strong>Silicon Valley</strong> to the vibrant startup scene in{" "}
-        <strong>Singapore's maritime industry.</strong> By day, I craft tasteful
-        front-end components and brainstorm ways to optimize sprawling backends
-        for multiple products at MarinaChain. By night, I am a tinkerer at
-        heart: you can find me writing indie music and making things prettier
-        via graphic design. I relish every opportunity to dive into complex
-        challenges, cherishing the process of creating elegant solutions that
-        captivate users with their intuitive design and seamless experiences.
-      </section>
+      <div className="space-y-40 p-48">
+        <section id="about" className="text-[#FFFFFF] font-light min-w-[600px]">
+          ABOUT <br />
+          My problem-solving journey began unexpectedly when I picked up Java as
+          an attempt to assist my high school principal with class scheduling. I
+          bore witness to the magic of what used to be months of intense manual
+          labor being reduced to a simple—and oh so satisfying—click of a
+          button; seeing how elegant code could efficiently streamline
+          processes, I was instantly hooked to the world of code. Since then,
+          I've ventured into the dynamic realm of startups, from the buzzing
+          landscape of <strong>Silicon Valley</strong> to the vibrant startup
+          scene in <strong>Singapore's maritime industry.</strong> By day, I
+          craft tasteful front-end components and brainstorm ways to optimize
+          sprawling backends for multiple products at MarinaChain. By night, I
+          am a tinkerer at heart: you can find me writing indie music and making
+          things prettier via graphic design. I relish every opportunity to dive
+          into complex challenges, cherishing the process of creating elegant
+          solutions that captivate users with their intuitive design and
+          seamless experiences.
+        </section>
 
-      <section id="experience" className="text-[#FFFFFF] p-48">
-        EXPERIENCE <br />
-        Here is the experience{/* your experience section content */}
-      </section>
+        <section
+          id="experience"
+          className="text-[#FFFFFF] font-light min-w-[600px] "
+        >
+          EXPERIENCE <br />
+          Here is the experience{/* your experience section content */}
+        </section>
 
-      <section id="projects" className="text-[#FFFFFF] p-48">
-        PROJECTS <br />
-        Here is the projects{/* your projects section content */}
-      </section>
+        <section
+          id="projects"
+          className="text-[#FFFFFF] font-light  min-w-[600px]"
+        >
+          PROJECTS <br />
+          Here is the projects{/* your projects section content */}
+        </section>
+      </div>
     </Layout>
   );
 };
