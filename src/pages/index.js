fdc35daf7768061126e0b8f8475e8804ebe2b06f
erynseo <yeryeon@gmail.com>
2023-08-20 16:30:45 +0800

    Include links to previous companies

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <div className="space-y-40 p-48">
         <section id="about" className="text-[#FFFFFF] font-light min-w-[600px]">
-          ABOUT <br />
           My problem-solving journey began unexpectedly when I picked up Java as
           an attempt to assist my high school principal with class scheduling. I
           bore witness to the magic of what used to be months of intense manual
@@ -15,22 +14,37 @@ const IndexPage = () => {
           button; seeing how elegant code could efficiently streamline
           processes, I was instantly hooked to the world of code. Since then,
           I've ventured into the dynamic realm of startups, from the buzzing
-          landscape of <strong>Silicon Valley</strong> to the vibrant startup
-          scene in <strong>Singapore's maritime industry.</strong> By day, I
-          craft tasteful front-end components and brainstorm ways to optimize
-          sprawling backends for multiple products at MarinaChain. By night, I
-          am a tinkerer at heart: you can find me writing indie music and making
-          things prettier via graphic design. I relish every opportunity to dive
-          into complex challenges, cherishing the process of creating elegant
-          solutions that captivate users with their intuitive design and
-          seamless experiences.
+          landscape of{" "}
+          <strong>
+            <a
+              href="https://www.propelsoftware.com/"
+              className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
+            >
+              Silicon Valley{" "}
+            </a>
+          </strong>
+          to the vibrant startup scene in{" "}
+          <strong>
+            <a
+              href="https://www.marinachain.io/"
+              className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
+            >
+              Singapore's maritime industry
+            </a>
+          </strong>
+          . By day, I craft tasteful front-end components and brainstorm ways to
+          optimize sprawling backends for multiple products at MarinaChain. By
+          night, I am a tinkerer at heart: you can find me writing indie music
+          and making things prettier via graphic design. I relish every
+          opportunity to dive into complex challenges, cherishing the process of
+          creating elegant solutions that captivate users with their intuitive
+          design and seamless experiences.
         </section>
 
         <section
           id="experience"
           className="text-[#FFFFFF] font-light min-w-[600px] "
         >
-          EXPERIENCE <br />
           Here is the experience{/* your experience section content */}
         </section>
 
@@ -38,7 +52,6 @@ const IndexPage = () => {
           id="projects"
           className="text-[#FFFFFF] font-light  min-w-[600px]"
         >
-          PROJECTS <br />
           Here is the projects{/* your projects section content */}
         </section>
       </div>
