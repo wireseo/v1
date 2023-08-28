bd236cd3a6292e79e47486da66e1419d565888bc
erynseo <yeryeon@gmail.com>
2023-08-28 09:21:32 +0800

    Add email icon to socials

--- src/components/socials.js
+++ src/components/socials.js
@@ -2,10 +2,15 @@ import * as React from "react";
 import { useState } from "react";
 import github from "../assets/icons/github-mark-white.svg";
 import linkedin from "../assets/icons/icons8-linkedin.svg";
+import email from "../assets/icons/mail-256.ico";
 
 const SocialIcon = ({ iconSrc, altText, profileLink }) => {
   const openProfile = () => {
-    window.open(profileLink, "_blank");
+    if (profileLink.startsWith("mailto:")) {
+      window.location.href = profileLink;
+    } else {
+      window.open(profileLink, "_blank");
+    }
   };
 
   //   TODO: Fix hover color for social icon
@@ -13,7 +18,7 @@ const SocialIcon = ({ iconSrc, altText, profileLink }) => {
     <img
       src={iconSrc}
       alt={altText}
-      className="w-5 h-5 icon cursor-pointer fill-current text-white hover:text-bright-blue transition-transform transform-gpu hover:scale-125"
+      className="w-7 h-7 md:w-5 md:h-5 icon cursor-pointer fill-current text-white hover:text-bright-blue transition-transform transform-gpu hover:scale-125"
       onClick={openProfile}
     />
   );
@@ -24,7 +29,7 @@ const Socials = ({ children }) => {
 
   return (
     <div
-      className="flex flex-col items-center p-10 space-y-3 absolute bottom-0 left-0"
+      className="flex md:flex-col ml-7 md:ml-0 items-center p-10 space-x-5 md:space-x-0 md:space-y-3 md:absolute md:bottom-0 md:left-0"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -38,10 +43,16 @@ const Socials = ({ children }) => {
         altText="LinkedIn"
         profileLink="https://www.linkedin.com/in/yeryeon-seo/"
       />
+      <SocialIcon
+        iconSrc={email}
+        altText="Email"
+        profileLink="mailto:yeryeon@gmail.com"
+      />
+
       <div
         className={`${
           isHovered ? "h-24" : "h-10"
-        } mb-4 w-px bg-bright-gray transition-height duration-300 ease-in-out`}
+        } mb-4 w-px bg-bright-gray transition-height duration-300 ease-in-out hidden md:block`}
       ></div>
     </div>
   );
