a1af07bbe7e22628c78f5f03619459dd7aaa6200
erynseo <yeryeon@gmail.com>
2023-09-06 11:36:46 +0800

    Remove animation and fix layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,8 @@
 import * as React from "react";
 import Tab from "./tab";
 import Socials from "./socials";
-import gsap from "gsap";
+
 import { useEffect, useState } from "react";
-import "../styles/cursorEffect.css";
 import "../styles/global.css";
 
 const Layout = ({ pageTitle, children }) => {
@@ -42,94 +41,62 @@ const Layout = ({ pageTitle, children }) => {
   }, []);
 
   useEffect(() => {
-    document.body.addEventListener("mousemove", (evt) => {
-      const mouseX = evt.pageX;
-      const mouseY = evt.pageY;
+    document.addEventListener("mousemove", (e) => {
+      const x = e.clientX;
+      const y = e.clientY;
 
-      gsap.set(".cursor", {
-        x: mouseX,
-        y: mouseY,
-      });
+      const gradient = `radial-gradient(500px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 70%)`;
 
-      gsap.to(".shape", {
-        x: mouseX,
-        y: mouseY,
-        ease: "power4.out",
-        stagger: -0.02,
-      });
+      document.querySelector(".gradient-spot").style.background = gradient;
     });
   }, []);
 
-  useEffect(() => {
-    const textLayer = document.querySelector(".text-layer");
-    const orb = document.querySelector(".orb");
-
-    if (textLayer && orb) {
-      const radius = textLayer.offsetWidth / 3;
-
-      gsap.to(orb, {
-        rotation: 360,
-        x: `+=${radius}`,
-        transformOrigin: `-${radius}px center`,
-        repeat: -1, // Infinite repetition
-        duration: 5, // Duration of a full circle
-        ease: "linear",
-      });
-    }
-  }, []);
-
   return (
-    <div className="bg-blue-black h-screen w-screen relative flex flex-col md:flex-row">
-      <div className="cursor"></div>
-      <div className="shapes absolute top-0 left-0 z-0">
-        <div className="shape shape-1"></div>
-        <div className="shape shape-2"></div>
-        <div className="shape shape-3"></div>
-      </div>
-
-      <div className=" md:basis-1/3">
-        <main>
-          <div className="flex flex-col pb-0 md:pb-16 p-16 relative min-w-[400px]">
-            <div className="text-layer">
-              <div className="orb"></div>
-              <h1 className="text-[#FFFFFF] text-6xl font-bold">Eryn Seo</h1>
-              <h2 className="text-[#FFFFFF] text-xl">
-                Tech Lead at MarinaChain
-              </h2>
-              <h3 className="text-[#d7dde680] text-sm font-light">
-                Inventive, elegant solutions for the web.
-              </h3>
+    <div className="bg-blue-black mx-auto min-h-screen min-w-screen px-24">
+      <div className="gradient-spot"></div>
+      <div className="lg:flex lg:justify-between lg:gap-10 lg:h-screen">
+        <div className="lg:w-1/2 lg:py-24 pt-12 lg:sticky lg:top-0">
+          <main>
+            <div className="flex flex-col pb-4 relative min-w-[300px]">
+              <div className="text-layer">
+                <h1 className="text-[#FFFFFF] text-6xl font-bold">Eryn Seo</h1>
+                <h2 className="text-[#FFFFFF] text-xl">
+                  Tech Lead at MarinaChain
+                </h2>
+                <h3 className="text-[#d7dde680] text-sm font-light">
+                  Inventive, elegant solutions for the web.
+                </h3>
+              </div>
             </div>
-          </div>
 
-          <nav className="pl-16 pt-8 hidden md:block">
-            <ul className="flex flex-col space-y-4">
-              <Tab
-                to="#about"
-                text="about"
-                isActive={activeTab === "about"}
-                setActiveTab={setActiveTab}
-              />
-              <Tab
-                to="#experience"
-                text="experience"
-                isActive={activeTab === "experience"}
-                setActiveTab={setActiveTab}
-              />
-              <Tab
-                to="#projects"
-                text="projects"
-                isActive={activeTab === "projects"}
-                setActiveTab={setActiveTab}
-              />
-            </ul>
-          </nav>
+            <nav className="pt-8 hidden lg:block">
+              <ul className="flex flex-col space-y-4">
+                <Tab
+                  to="#about"
+                  text="about"
+                  isActive={activeTab === "about"}
+                  setActiveTab={setActiveTab}
+                />
+                <Tab
+                  to="#experience"
+                  text="experience"
+                  isActive={activeTab === "experience"}
+                  setActiveTab={setActiveTab}
+                />
+                <Tab
+                  to="#projects"
+                  text="projects"
+                  isActive={activeTab === "projects"}
+                  setActiveTab={setActiveTab}
+                />
+              </ul>
+            </nav>
 
-          <Socials />
-        </main>
+            <Socials />
+          </main>
+        </div>
+        <div className="overflow-auto lg:w-1/2">{children}</div>
       </div>
-
-      <div className="overflow-auto md:basis-2/3">{children}</div>
     </div>
   );
 };
