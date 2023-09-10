71e787c3743e79e10a238a71822208886a94b47f
erynseo <yeryeon@gmail.com>
2023-09-10 20:29:58 +0800

    Fix wave name componen size

--- src/components/waveName.js
+++ src/components/waveName.js
@@ -3,6 +3,7 @@ import React from "react";
 const WaveName = () => {
   return (
     <svg
+      width="520"
       viewBox="0 0 800 100"
       xmlns="http://www.w3.org/2000/svg"
       shape-rendering="geometricPrecision"
