a5c6702b08582a9583f5b3a4c7da10eabd217c82
erynseo <yeryeon@gmail.com>
2023-09-12 11:15:30 +0800

    Fix responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,55 +10,57 @@ const IndexPage = () => {
       <div className="space-y-20 lg:space-y-60 py-24">
         <section
           id="about"
-          className="text-[#FFFFFF]/[0.7] font-light min-w-[550px] leading-relaxed"
+          className="text-[#FFFFFF]/[0.7] font-light leading-relaxed"
         >
-          My problem-solving journey began unexpectedly when I picked up Java as
-          an attempt to assist my high school principal with class scheduling. I
-          bore witness to the magic of what used to be months of intense manual
-          labor being reduced to a simple—and oh so satisfying—click of a
-          button; seeing how elegant code could efficiently streamline
-          processes, I was instantly hooked to the world of code. <br />
+          <p>
+            My problem-solving journey began unexpectedly when I picked up Java
+            as an attempt to assist my high school principal with class
+            scheduling. I bore witness to the magic of what used to be months of
+            intense manual labor being reduced to a simple—and oh so
+            satisfying—click of a button; seeing how elegant code could
+            efficiently streamline processes, I was instantly hooked to the
+            world of code.{" "}
+          </p>
           <br />
-          Since then, I've ventured into the dynamic realm of startups, from the
-          buzzing landscape of{" "}
-          <strong>
-            <a
-              href="https://www.propelsoftware.com/"
-              className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
-            >
-              Silicon Valley{" "}
-            </a>
-          </strong>
-          to the vibrant startup scene in{" "}
-          <strong>
-            <a
-              href="https://www.marinachain.io/"
-              className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
-            >
-              Singapore's maritime industry
-            </a>
-          </strong>
-          . By day, I craft tasteful front-end components and brainstorm ways to
-          optimize sprawling backends for multiple products at MarinaChain. By
-          night, I am a tinkerer at heart: you can find me writing{" "}
-          <strong>
-            <a
-              href="https://open.spotify.com/artist/0w1g0B53ZzbTkbev0WDAmm?si=BMuVaWz-TZWUe0c7QdW32A"
-              className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
-            >
-              indie music
-            </a>
-          </strong>{" "}
-          and making things prettier via graphic design. I relish every
-          opportunity to dive into complex challenges, cherishing the process of
-          creating elegant solutions that captivate users with their intuitive
-          design and seamless experiences.
+          <p>
+            Since then, I've ventured into the dynamic realm of startups, from
+            the buzzing landscape of{" "}
+            <strong>
+              <a
+                href="https://www.propelsoftware.com/"
+                className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
+              >
+                Silicon Valley{" "}
+              </a>
+            </strong>
+            to the vibrant startup scene in{" "}
+            <strong>
+              <a
+                href="https://www.marinachain.io/"
+                className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
+              >
+                Singapore's maritime industry
+              </a>
+            </strong>
+            . By day, I craft tasteful front-end components and brainstorm ways
+            to optimize sprawling backends for multiple products at MarinaChain.
+            By night, I am a tinkerer at heart: you can find me writing{" "}
+            <strong>
+              <a
+                href="https://open.spotify.com/artist/0w1g0B53ZzbTkbev0WDAmm?si=BMuVaWz-TZWUe0c7QdW32A"
+                className="text-bright-blue opacity-80 hover:opacity-100 transition-opacity duration-300"
+              >
+                indie music
+              </a>
+            </strong>{" "}
+            and making things prettier via graphic design. I relish every
+            opportunity to dive into complex challenges, cherishing the process
+            of creating elegant solutions that captivate users with their
+            intuitive design and seamless experiences.
+          </p>
         </section>
 
-        <section
-          id="experience"
-          className="text-[#FFFFFF] font-light min-w-[550px]"
-        >
+        <section id="experience" className="text-[#FFFFFF] font-light">
           <ExperienceProvider>
             <ExperienceCard
               startDate="Sep 2022"
