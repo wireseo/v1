158afda37e3e5237be4674b93a6fc5df4e8ada1f
erynseo <yeryeon@gmail.com>
2023-10-25 13:26:33 +0900

    Add selection effects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import Projects from "./projects";
 const IndexPage: React.FC = () => {
   return (
     <Layout pageTitle="Home Page">
-      <div className="space-y-28 lg:space-y-40 py-32">
+      <div className="space-y-28 lg:space-y-40 py-32 selection:bg-bright-blue selection:text-blue-black">
         <About />
         <Experience />
         <Projects />
